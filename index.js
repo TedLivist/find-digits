621d7cd4f4c8ce26f9c24eaa4caafadb6819b80e
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-04-18 17:29:29 +0100

    Create index.js and add solution

---
+++ index.js
@@ -0,0 +1,12 @@
+function findDigits(num) {
+  let numToString = num.toString()
+  let numIntoArray = numToString.split('')
+  let divisorCount = 0
+  for (let  i = 0; i < numIntoArray.length; i++) {
+    numIntoArray[i] = parseInt(numIntoArray[i])
+    if (num % numIntoArray[i] == 0) {
+      divisorCount += 1
+    }
+  }
+  return divisorCount
+}
